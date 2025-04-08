5a52f6c8f20edd9b0d3d731f99e2a1567c0747fa
gigi2-Mamba <68654070+gigi2-Mamba@users.noreply.github.com>
2025-04-08 23:39:52 +0800

    final commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,17 +2,17 @@ const { ethers, upgrades } = require("hardhat");
 
 async function tokenDeploy() {
   const Token = await ethers.getContractFactory("matchToken");
-  const token = await Token.deploy("M1", "m1");
+  const token = await Token.deploy("MATCH0", "m0");
   await token.waitForDeployment();
-  console.log("Token deployed to:", token.target); // 0xBC649E21df0fCf476C0eFE5Cb8339FA9f756d7Eb
+  console.log("Token deployed to:", token.target); //
 }
 
-tokenDeploy();
+// tokenDeploy();
 
 async function upgradeDeploy() {
   // const [deployer] = await ethers.getSigners();
   // console.log("Deploying contracts with the account:", deployer.address);
-  const erc20 = "0xBC649E21df0fCf476C0eFE5Cb8339FA9f756d7Eb";
+  const erc20 = "0xffbc2aA0bf5B6f722a98Cc0563968134227Ca4a1";
   const protocol = "0x8048546982F7cF509a1A188B8A7eF265D1c2Ca1f";
   const st = 360;
   const et = 540;
@@ -21,7 +21,7 @@ async function upgradeDeploy() {
     initializer: "initialize",
   });
   await token.waitForDeployment();
-  console.log("Token address:", token.target); //0x5525cc1EB3A7F0c2D6d5869E50f471dF0Ce46160
+  console.log("Token address:", token.target); //0x543f682BEd4458Ec024528b228052e5723D9531B
 }
 
 upgradeDeploy()
